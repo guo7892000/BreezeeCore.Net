e7eabe835981e013e45a273c5c076519b15ded0c
guohui.huang <guo7892000@126.com>
2023-09-07 23:21:34 +0800

    不覆盖原压缩包

--- 001_BreezeeCore.Net/Breezee.Core.Tool/Helper/FileDirHelper.cs
+++ 001_BreezeeCore.Net/Breezee.Core.Tool/Helper/FileDirHelper.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -125,41 +126,99 @@ namespace Breezee.Core.Tool.Helper
         public static async Task DownloadWebZipAndUnZipAsync(string sSoureUrl, string sLocalDirPath)
         {
             //下载地址：https://gitee.com/breezee2000/WorkHelper/releases/download/1.2.24/WorkHelper1.2.24.rar
-            Regex regex = new Regex(@"/(\w|\.)+.(rar|zip|7z)", RegexOptions.IgnoreCase);
+            Regex regex = new Regex(@"/(\w|\.)+.(rar|zip|7z)", RegexOptions.IgnoreCase); //得到绿色版包名
             MatchCollection mc = regex.Matches(sSoureUrl);
             if (mc.Count > 0)
             {
                 string sFileName = mc[0].Value.Substring(1);
+                string sDirName = mc[0].Value.Substring(1, sFileName.LastIndexOf("."));
                 string sFullZipPath = Path.Combine(sLocalDirPath, sFileName);
-                if (!File.Exists(sFullZipPath))
+                //如已存在，修改压缩包名称：如删除文件，杀毒软件会误报为病毒
+                if (File.Exists(sFullZipPath))
                 {
-                    //先下载到临时文件
-                    var tmp = sFullZipPath + ".tmp";
-                    using (var web = new WebClient())
+                    //找出版本号
+                    regex = new Regex(@"(\d+.)+", RegexOptions.IgnoreCase);
+                    mc = regex.Matches(sFileName);
+                    if (mc.Count > 0)
                     {
-                        await web.DownloadFileTaskAsync(sSoureUrl, tmp);
-                    }
-                    //重命名文件
-                    File.Move(tmp, sFullZipPath);
-                    //解压文件:只针对.zip文件，不能解压.rar格式
-                    //ZipFile.ExtractToDirectory(sFullZipPath, sLocalDirPath, Encoding.UTF8);
-                    //解压的文件
-                    using (Stream stream = File.OpenRead(sFullZipPath))
-                    { 
-                        var reader = ReaderFactory.Open(stream);
-                        while (reader.MoveToNextEntry())
+                        //有版本号
+                        string sVersion = mc[0].Value.Substring(0, mc[0].Value.Length-1);
+                        string sSetupName = sFileName.Substring(0, mc[0].Index);
+                        string sExtName = sFileName.Substring(mc[0].Index+ mc[0].Length);
+                        int iNewFile = 1;//从所有父目录查找同名文件还有没有
+                        while (File.Exists(Path.Combine(sLocalDirPath, sSetupName+ sVersion+"("+ iNewFile +")."+ sExtName)))
                         {
-                            if (!reader.Entry.IsDirectory)
+                            iNewFile++;
+                        }
+                        sFileName = sSetupName + sVersion + "(" + iNewFile + ")." + sExtName;
+                        sFullZipPath = Path.Combine(sLocalDirPath, sFileName);
+                    }
+                    else
+                    {
+                        //无版本号，直接取压缩包后缀
+                        regex = new Regex(@".(rar|zip\7z)$", RegexOptions.IgnoreCase);
+                        mc = regex.Matches(sFileName);
+                        if (mc.Count > 0)
+                        {
+                            string sSetupName = sFileName.Substring(0, mc[0].Index);
+                            int iNewFile = 1;
+                            while (File.Exists(Path.Combine(sLocalDirPath, sSetupName +"(" + iNewFile + ")" + mc[0].Value)))
                             {
-                                Console.WriteLine(reader.Entry.Key);
-                                reader.WriteEntryToDirectory(sLocalDirPath, new ExtractionOptions()
-                                {
-                                    ExtractFullPath = true,
-                                    Overwrite = true
-                                });
+                                iNewFile++;
                             }
+                            sFileName = sSetupName + "(" + iNewFile + ")" + mc[0].Value;
+                            sFullZipPath = Path.Combine(sLocalDirPath, sFileName);
+                        }
+                    }                    
+                }
+                //下载到临时文件
+                var tmpZipName = sFullZipPath + ".tmp";
+                using (var web = new WebClient())
+                {
+                    await web.DownloadFileTaskAsync(sSoureUrl, tmpZipName);
+                }
+
+                //重命名文件
+                File.Move(tmpZipName, sFullZipPath);
+                //解压文件:只针对.zip文件，不能解压.rar格式
+                //ZipFile.ExtractToDirectory(sFullZipPath, sLocalDirPath, Encoding.UTF8);
+                //解压的文件
+                using (Stream stream = File.OpenRead(sFullZipPath))
+                {
+                    var reader = ReaderFactory.Open(stream);
+                    while (reader.MoveToNextEntry())
+                    {
+                        if (!reader.Entry.IsDirectory)
+                        {
+                            Console.WriteLine(reader.Entry.Key);
+                            reader.WriteEntryToDirectory(sLocalDirPath, new ExtractionOptions()
+                            {
+                                ExtractFullPath = true,
+                                Overwrite = true
+                            });
                         }
                     }
+                }
+                //备份数据库文件
+                try
+                {
+                    //SQLite的数据库文件也复制一份到目标目录：如表结构有变更
+                    DirectoryInfo dirNow = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+                    string dbFile = Path.Combine(dirNow.FullName, "SQLite_DBTool.db");
+                    string sNewDir = Path.Combine(sLocalDirPath, sDirName);
+                    if (File.Exists(dbFile))
+                    {
+                        CopyFilesToDirKeepSrcDirName(dbFile, sNewDir);
+                    }
+                    dbFile = Path.Combine(dirNow.FullName, "SQLite_Mini.db");
+                    if (File.Exists(dbFile))
+                    {
+                        CopyFilesToDirKeepSrcDirName(dbFile, sNewDir);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message); //复制文件出错，只在控制台输入错误信息
                 }
             }
         }
