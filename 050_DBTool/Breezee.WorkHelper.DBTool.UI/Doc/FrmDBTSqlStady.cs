b937525988a6ce7f18cd2bcb6a175a919485e173
guohui.huang <guo7892000@126.com>
2024-03-11 00:04:25 +0800

    11

--- 050_DBTool/Breezee.WorkHelper.DBTool.UI/Doc/FrmDBTSqlStady.cs
+++ 050_DBTool/Breezee.WorkHelper.DBTool.UI/Doc/FrmDBTSqlStady.cs
@@ -10,7 +10,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Contexts;
+//using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
